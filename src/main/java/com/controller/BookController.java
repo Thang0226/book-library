5c8613a55532a8ddb487c19a9fca2dedccc94556
Thang0226 <thang.nd0226@gmail.com>
2024-12-27 17:42:52 +0700

    can view & confirm to borrow book

--- src/main/java/com/controller/BookController.java
+++ src/main/java/com/controller/BookController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/books")
@@ -31,5 +34,12 @@ public class BookController {
     public String saveBook(Book book) {
         bookService.save(book);
         return "redirect:/books";
+    }
+
+    @GetMapping("/view/{id}")
+    public String showViewForm(@PathVariable("id") Long id, Model model) {
+        Optional<Book> book = bookService.findById(id);
+        model.addAttribute("book", book.get());
+        return "book/view";
     }
 }
